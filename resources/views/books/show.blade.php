8f479400e4af0dfe95608e102dc411cf88980d07
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2023-05-20 20:34:03 +0330

    Show Book Comments

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -2,5 +2,12 @@
 @section('content')
 <h1>{{ $book->title }}</h1>
 <hr>
+Comments
+<hr>
+<ul>
+@foreach ( $book->bookcomments as $comment )
+    <li>{{ $comment->fullname  }}</li>
+@endforeach
+</ul>
 
 @endsection
