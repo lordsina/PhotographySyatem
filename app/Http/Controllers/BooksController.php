19e3d77663fe67dcd3fe1d5a23f29296135a9d33
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2023-06-09 16:02:00 +0330

    Middleware

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -15,6 +15,7 @@ class BooksController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        //$this->middleware('admin');
     }
     
     public function index()
---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $user=$request->user();
+        if($user && $user->phone=='09364900199'){
+            return $next($request);
+        }
+
+        abort(404,'NoWay');
+
+    }
+}
