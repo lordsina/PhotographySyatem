439ed0b75d9bca98d6ee42ff2f41731ca526a51a
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2023-06-17 23:33:12 +0330

    Dashboard Ui Change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,15 +2,28 @@
 @section('content')
 <div class="row align-items-start">
     <div class="col">
-        @foreach ($users as $user)
-            {{ $user->firstname }}
-        @endforeach
+      <div class="m-2 p-1 rounded bg-info text-light" style="height: 100px;">
+        <p>تعداد کل یوزهای سایت</p>
+        <h5>{{ $users->count() }}</h5>
+      </div>
+    </div>
+    <div class="col" >
+      <div class="m-2 p-1 rounded bg-warning text-light" style="height: 100px;">
+        <p>
+          ثبت نام اخرین کاربر
+        </p>
+        <p>
+          @foreach ($users as $user)
+            {{ $user->firstname.$user->lastname }}
+          @endforeach
+        </p>
+
+      </div>
     </div>
     <div class="col">
-      One of three columns
-    </div>
-    <div class="col">
-      One of three columns
+      <div class="m-2 p-1 rounded bg-danger text-light" style="height: 100px;">
+
+      </div>
     </div>
   </div>
   <hr>
