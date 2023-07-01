d0683d48913ac20edecee29c12735e6d0aa42b58
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2023-07-01 23:54:54 +0330

    faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->name(),
+            'lastname' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->unique()->phoneNumber(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '123456', // password
             'remember_token' => Str::random(10),
         ];
     }
