8643a26b88ea9ac31312b9828ddc12119ae748a8
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2023-08-07 23:04:01 +0330

    clean some codes

--- database/seeders/BookCommentSeeder.php
+++ database/seeders/BookCommentSeeder.php
@@ -15,23 +15,7 @@ class BookCommentSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-    //     Book::factory()
-    //     ->has(
-    //         Bookcomment::factory()
-    //         ->count(5)
-    //         //->state(function (array $attributes,User $user){
-    //        //     return ['user_id'=>$user->all()->random()];
-    //    // })
-    //     )
-    //     ->create();
-
-        // $book=Book::factory()->create();
-
-        // $bookcomments=Bookcomment::factory()
-        // ->count(3)
-        // ->for($book)
-        // ->create();
-        
+    {   
+        // In Book 
     }
 }
